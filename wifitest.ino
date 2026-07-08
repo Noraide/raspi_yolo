24c1991a965d2b08f300820ae046abd55ba4ef6d
Noraide <165112837+Noraide@users.noreply.github.com>
2026-07-08 15:15:29 +0800

    Create wifitest.ino

---
+++ wifitest.ino
@@ -0,0 +1,32 @@
+//#include <ESP8266WiFi.h>
+#include <WiFi.h>
+
+const char* ssid = "MASUKKAN NAMA WIFI";
+const char* password = "MASUKKAN PASSWORD";
+
+void setup() {
+  Serial.begin(115200);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password); // masuk wifi
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+ 
+}
